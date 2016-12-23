fb47f5298068c9c680df8bbea89b3381f1caeb85
eascherman <ernie.ascherman@gmail.com>
2016-12-23 11:26:12 -0500

    Delete appendExampleCode.js

--- appendExampleCode.js
+++
@@ -1,8 +0,0 @@
-
-var example = document.getElementById('example');
-
-var scriptDisplay = re.bundle
-    `<hr />
-    <pre>${example.childNodes[0].textContent}</pre>`;
-
-re.install(scriptDisplay, document.body)
